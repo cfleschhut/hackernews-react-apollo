8694bfd775dc088b35ab5c52e69e7cecbf72488b
Christian Fleschhut <weltrekordhalter@gmail.com>
2017-11-26 21:11:57 +0100

    Configure ApolloClient

--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cjah4slj20dbz0179lwkywj16'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
