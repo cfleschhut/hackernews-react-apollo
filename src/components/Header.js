457ffacb0e159b73ed75ab39fa86c0cc7bf0a525
Christian Fleschhut <weltrekordhalter@gmail.com>
2017-11-29 21:52:02 +0100

    Add Login component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import { GC_USER_ID, GC_AUTH_TOKEN } from '../constants';
 
-const Header = () =>
-  <div className="flex pa1 justify-between nowrap orange">
-    <div className="flex flex-fixed black">
-      <div className="fw7 mr1">
-        Hacker News
+const Header = () => {
+  const userId = localStorage.getItem(GC_USER_ID);
+
+  return (
+    <div className="flex pa1 justify-between nowrap orange">
+      <div className="flex flex-fixed black">
+        <div className="fw7 mr1">
+          Hacker News
+        </div>
+        <Link to="/" className="ml1 no-underline black">
+          New
+        </Link>
+        {userId &&
+          <div className="flex">
+            <div className="ml1">|</div>
+            <Link to="/create" className="ml1 no-underline black">
+              Submit
+            </Link>
+          </div>
+        }
       </div>
-      <Link to="/" className="ml1 no-underline black">
-        new
-      </Link>
-      <div className="ml1">|</div>
-      <Link to="/create" className="ml1 no-underline black">
-        submit
-      </Link>
+      <div className="flex flex-fixed">
+        {userId ?
+          <div className="ml1 pointer black" onClick={() => {
+            localStorage.removeItem(GC_USER_ID);
+            localStorage.removeItem(GC_AUTH_TOKEN);
+
+            this.props.history.push(`/new/1`);
+          }}>
+            Logout
+          </div> :
+          <Link to="/login" className="ml1 no-underline black">
+            Login
+          </Link>
+        }
+      </div>
     </div>
-  </div>
+  );
+}
 
 export default withRouter(Header)
